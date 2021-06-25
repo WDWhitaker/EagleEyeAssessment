ced1ffa2c003ea1c85ad35b805120266f49c1b4c
wesley whitaker <wesleydanielwhitaker@gmail.com>
2021-06-25 13:04:00 +0100

    Add project files.

---
+++ EagleEyeAssessment/Models/MovieStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EagleEyeAssessment.Models
+{
+    public class MovieStats
+    {
+        //movieId,watchDurationMs
+
+        public int MovieId { get; set; }
+        public decimal WatchDurationMs { get; set; }
+    }
+}
